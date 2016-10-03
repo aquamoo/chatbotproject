878efd6f4dc7b4acef308ecfe28f11a22e74035e
Jordan Zach <jdzach2000@gmail.com>
2016-10-03 12:02:29 -0700

    added enter key ability
    now works when presssing enter

--- chatbot.js
+++ chatbot.js
@@ -1,13 +1,38 @@
 function askAQuestion() {
   var input = document.getElementById("input").value;
+  input = input.toLowerCase()
   var giveresponse = responses[input]
+
+
+
+  if (giveresponse == undefined) {
+document.getElementById('text-area').innerHTML += "sorry I don't understand <br/> <br/>";
+  }
+  else {
+
+
   document.getElementById("text-area").innerHTML += input + ": " + giveresponse + " ";
+  }
 }
 
 var responses = {
-  "Hi" : "What up? <br/> <br/>",
-  "How are you" : "Get out of my swamp! <br/> <br/>",
-  "Whats up" : "Gas prices <br/> <br/>",
-  "Can you help me with my code" : "I don't get payed enough for that <br/> <br/>",
-  "" : ""
+  "how are you" : "Get out of my swamp! <br/> <br/>",
+  "whats up" : "Gas prices <br/> <br/>",
+  "can you help me with my code" : "I don't get payed enough for that <br/> <br/>",
+  "hi" : "Get out of my swamp! <br/> <br/>",
+  "r u a g" : "yee ima 2 c00l 4 u <br/> <br/>",
+  "omg r u really shrek" : "yep <br/> <br/>",
+  "whats your favorite game" : "ogre-watch <br/> <br/>",
+  "how old are you" : "im ogre 9000 <br/> <br/>",
+  "what is your name" : "SHREK <br/> <br/>",
+  "what time is it" : "look at the bottom right of your screen <br/> <br/>",
+  "did you did good in school" : "yeah i was a ogre achiever <br/> <br/>",
+  "how do you clean up messes" : "i use disinshrektant <br/> <br/>",
+  "do you have any good quotes" : "check yourself befor you shrek yourself <br/> <br/>"
 }
+
+$(document).keydown(function(e) {
+ if (e.keyCode == 13) {
+askAQuestion();
+ }
+});
